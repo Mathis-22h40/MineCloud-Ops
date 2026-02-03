d5fc82e20863f4abba01fe9f3d96b4432d349587
Mathis-22h40 <mathis@22h40.com>
2026-02-03 16:06:34 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,15 @@
 # terraform/main.tf
 
-# Exemple pour VMware Workstation (provider local simple)
-# Si tu n'as pas vSphere, Terraform sur VMware Workstation est limité.
-# Le plus simple pour le TP est de simuler ou d'utiliser le provider "vmworkstation" si tu l'as installé.
-
 provider "vmworkstation" {
-    # Configuration locale
 }
 
 resource "vmworkstation_vm" "minecraft_server" {
-    source_vm = "minecloud-base-v1" # L'image créée par Packer [cite: 47]
+    source_vm = "minecloud-base-v1" 
     name      = "prod-minecraft"
     processors = 2 # [cite: 48]
     memory    = 4096 # [cite: 48]
 }
 
 output "ip_address" {
-    value = vmworkstation_vm.minecraft_server.ip # [cite: 49]
+    value = vmworkstation_vm.minecraft_server.ip 
 }
